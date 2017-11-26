74e64e0e9d8ac393be0b89a87073f394f3e22f2a
Kevin Mingie Kim <kkim7370@terpmail.umd.edu>
2017-11-26 14:49:09 -0500

    Revert "Merge branch 'master' of github.com:gizmoe/ContextAware into sqliteStuff"
    This reverts commit b29144554d738f0ccec7221634b7b7ac3817a185, reversing
    changes made to 9b907452fcbd0ab9f5da2b88dd4f7f02e905093d.

--- app/src/main/java/com/maryland/cmsc436/contextaware/MainActivity.java
+++ app/src/main/java/com/maryland/cmsc436/contextaware/MainActivity.java
@@ -1,6 +1,5 @@
 package com.maryland.cmsc436.contextaware;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,9 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent myIntent = new Intent(getApplicationContext(),AddNewContext.class);
-        startActivity(myIntent);
     }
-
-//testing hello
 }
