66a3b132ce82d79e830026500df03e67cef0b372
Kevin Mingie Kim <kkim7370@terpmail.umd.edu>
2017-11-29 14:07:31 -0500

    Implementation for most of settings queries

--- app/src/main/java/com/maryland/cmsc436/contextaware/DBAccess.java
+++ app/src/main/java/com/maryland/cmsc436/contextaware/DBAccess.java
@@ -1,5 +1,6 @@
 package com.maryland.cmsc436.contextaware;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -20,8 +21,15 @@ public class DBAccess extends SQLiteOpenHelper {
 	private static final int DB_VERSION = 1;
 
 	private static final String SETTING_TABLE = "settings";
+	private static final String SETTING_TITLE = "title";
+	private static final String SETTING_RINGER = "ringer";	//0=SILENT, 1=VIBRATE, 2=RING
+	private static final String SETTING_ACTIVE = "enabled";	//0=DISABLED, 1=ENABLED
 
 	private static final String LOC_TABLE = "locations";
+	private static final String LOC_ADDRESS = "address";
+	private static final String LOC_LAT = "latitude";
+	private static final String LOC_LONG = "longitude";
+	private static final String LOC_SID = "sid";
 
 
 	//Factory method to get the object
@@ -37,36 +45,71 @@ public class DBAccess extends SQLiteOpenHelper {
 	}
 
 	public void onCreate(SQLiteDatabase db) {
-		String createLocTables = "";
-		String createSettingTables = "";
-		//TODO
+		String createSettingTables = "CREATE TABLE IF NOT EXISTS " + SETTING_TABLE + " ("
+				+ SETTING_TITLE + " TEXT NOT NULL, "
+				+ SETTING_RINGER + " INTEGER, "
+				+ SETTING_ACTIVE + " INTEGER "
+				+ ");";
+		String createLocTables = "CREATE TABLE IF NOT EXISTS" + LOC_TABLE + " ("
+				+ LOC_ADDRESS + " TEXT NOT NULL,"
+				+ LOC_LAT + " INTEGER, "
+				+ LOC_LONG + " INTEGER, "
+				+ LOC_SID + "INTEGER, "
+				+ "PRIMARY KEY (" + LOC_LAT + ", " + LOC_LONG + ") "
+				+ ");";
 
 		db.execSQL(createLocTables);
 		db.execSQL(createSettingTables);
 
 	}
 	public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer) {
-		db.execSQL("DROP TABLE IF EXISTS " + SETTING_TABLE);
-		db.execSQL("DROP TABLE IF EXISTS " + LOC_TABLE);
+		db.execSQL("DROP TABLE IF EXISTS " + SETTING_TABLE + ";");
+		db.execSQL("DROP TABLE IF EXISTS " + LOC_TABLE + ";");
 
 		onCreate(db);
 	}
 
 
 	//Information used to display in a list
-	public ArrayList getAllSettings() {
-		String settingSelect = "";
-		ArrayList toReturn = new ArrayList();
+	public ArrayList<ContextSettings> getAllSettings() {
+		String[] cols = {SETTING_TITLE, SETTING_RINGER, SETTING_ACTIVE};
+		ArrayList<ContextSettings> toReturn = new ArrayList<ContextSettings>();
 		SQLiteDatabase db = getReadableDatabase();
-		//TODO
 
-		//Cursor data = db.query(SETTING_TABLE,String[] cols, sel, selargs, groupby, having, orderby)
-		//int listSize = data.getCount();
-		//data.moveToFirst();
-		//while (data.isLast() != true) {
-		//data.moveToNext();
+		Cursor data = db.query(SETTING_TABLE, cols, null, null, null, null, SETTING_TITLE + " DESC");
+		int listSize = data.getCount();
+		data.moveToFirst();
 
-        return null;
+		while (data.isAfterLast() != true) {
+			String curTitle = data.getString(data.getColumnIndex(SETTING_TITLE));
+			ContextSettings.ActiveStatus st;
+			ContextSettings.Ringer ring;
+
+			switch (data.getInt(data.getColumnIndex(SETTING_RINGER))) {
+				case 0:
+					ring = ContextSettings.Ringer.SILENT;
+					break;
+				case 1:
+					ring = ContextSettings.Ringer.VIBRATE;
+					break;
+				default: //case 2:
+					ring = ContextSettings.Ringer.LOUD;
+					break;
+			}
+			switch (data.getInt(data.getColumnIndex(SETTING_ACTIVE))) {
+				case 0:
+					st = ContextSettings.ActiveStatus.NO;
+					break;
+				default: //case 1:
+					st = ContextSettings.ActiveStatus.YES;
+					break;
+			}
+
+			toReturn.add(new ContextSettings(curTitle, ring, st));
+			data.moveToNext();
+		}
+
+        return toReturn;
 	}
 
 	//Information to display in a list
@@ -90,9 +133,32 @@ public class DBAccess extends SQLiteOpenHelper {
         return null;
     }
 
-    public void updateSetting() {
-		String settingUpdate = "";
+    public void updateSetting(ContextSettings changedSettings) {
+		SQLiteDatabase db = getWritableDatabase();
+		ContentValues updates = new ContentValues();
 
+		switch (changedSettings.getRinger()) {
+			case SILENT:
+				updates.put(SETTING_RINGER, 0);
+				break;
+			case VIBRATE:
+				updates.put(SETTING_RINGER, 1);
+				break;
+			default: //case LOUD:
+				updates.put(SETTING_RINGER, 2);
+				break;
+		}
+
+		switch (changedSettings.getStatus()) {
+			case YES:
+				updates.put(SETTING_ACTIVE, 1);
+				break;
+			default: //case NO:
+				updates.put(SETTING_ACTIVE, 0);
+				break;
+		}
+
+		db.update(SETTING_TABLE, updates, SETTING_TITLE + " = ?", new String[] { changedSettings.getTitle() });
     }
 
     public void updatePlace() {
